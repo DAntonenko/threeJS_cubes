ae9ce5eb18a5ff75bc0109f1a0b3219ca9a18252
Danton <dm.antonenko1977@gmail.com>
2020-01-13 01:47:40 +0300

    raycasting added

--- cubes.js
+++ cubes.js
@@ -51,8 +51,20 @@ document.body.appendChild(renderer.domElement);
 const cube = new Cube();
 
 scene.add(cube);
-
 camera.position.z = 5;
+
+// Raycasting
+let raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+function onMouseMove(event) {
+	// calculate mouse position in normalized device coordinates
+	// (-1 to +1) for both components
+
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  console.log(event);
+}
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -60,9 +72,19 @@ const animate = () => {
   cube.rotation.x += 0.001;
   cube.rotation.y += 0.001;
 
+  // update the picking ray with the camera and mouse position
+	raycaster.setFromCamera(mouse, camera);
+
+	// calculate objects intersecting the picking ray
+	let intersects = raycaster.intersectObjects(scene.children);
+
+	for (let i = 0; i < intersects.length; i++) {
+		intersects[i].object.material.color.set( 0xff0000 );
+	}
+
   renderer.render(scene, camera);
 };
 
-// console.log(spheres);
+window.addEventListener('click', onMouseMove, false);
 
 animate();
