ff883f1e522e54316536a77522e0899c4a212e7c
Danton <dm.antonenko1977@gmail.com>
2020-01-14 03:47:50 +0300

    ugly, but almost works; single cube yet

--- cubes.js
+++ cubes.js
@@ -1,5 +1,6 @@
 // Serial numbers of spheres (in 'Cube.spheres' array) which mast be connected with edge lines
 const edges = [[0, 1], [1, 3], [2, 0], [3, 2], [4, 5], [5, 7], [6, 4], [7, 6], [0, 4], [1, 5], [2, 6], [3, 7]];
+let lines = [];
 
 class Sphere extends THREE.Mesh {
   constructor(x, y, z, color) {
@@ -34,6 +35,7 @@ class Cube extends THREE.Object3D {
         new THREE.Vector3(this.spheres[verticesPair[1]].x, this.spheres[verticesPair[1]].y, this.spheres[verticesPair[1]].z)
       );
       this.add(new THREE.Line( geometry, new THREE.LineBasicMaterial({color: 0xfff40b})));
+      lines.push(geometry.vertices);
     });
 
     // this.rotation.x += 0.5;
@@ -63,7 +65,52 @@ function onMouseMove(event) {
 
 	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
   mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-  console.log(event);
+
+  setColor()
+}
+
+function setColor() {
+  // update the picking ray with the camera and mouse position
+	raycaster.setFromCamera(mouse, camera);
+
+	// calculate objects intersecting the picking ray
+	let intersects = raycaster.intersectObjects(scene.children, true);
+
+	for (let i = 0; i < intersects.length; i++) {
+    // intersects[i].object.material.color.set( 0xff0000 );
+    if (intersects[i].object instanceof Sphere) {
+      lines.forEach((item) => {
+        if ((item[0].x === intersects[i].object.position.x &&
+          item[0].y === intersects[i].object.position.y &&
+          item[0].z === intersects[i].object.position.z)
+          ||
+          (item[1].x === intersects[i].object.position.x &&
+          item[1].y === intersects[i].object.position.y &&
+          item[1].z === intersects[i].object.position.z)) {
+
+          // ***
+
+          let geometry = new THREE.Geometry();
+          geometry.vertices.push(
+            new THREE.Vector3(item[0].x, item[0].y, item[0].z),
+            new THREE.Vector3(item[1].x, item[1].y, item[1].z)
+          );
+          // console.log(intersects[i].object.material.color);
+          const edgeColor = `rgb(${Math.floor(intersects[i].object.material.color.r)},${Math.floor(intersects[i].object.material.color.g)},${Math.floor(intersects[i].object.material.color.b)})`;
+
+          cube.add(new THREE.Line( geometry, new THREE.LineBasicMaterial({color: edgeColor})));
+
+          // ***
+        }
+        // console.log(item[0]);
+        // console.log(intersects[i].object.position);
+      });
+      // console.log(intersects[i].object.uuid);
+      
+      // console.log(this.lines);
+      // this.THREE.Line.vertices[0] === intersects[i].object.position && console.log(this.THREE.Line.uuid);
+    }
+	}
 }
 
 const animate = () => {
@@ -71,16 +118,6 @@ const animate = () => {
 
   cube.rotation.x += 0.001;
   cube.rotation.y += 0.001;
-
-  // update the picking ray with the camera and mouse position
-	raycaster.setFromCamera(mouse, camera);
-
-	// calculate objects intersecting the picking ray
-	let intersects = raycaster.intersectObjects(scene.children);
-
-	for (let i = 0; i < intersects.length; i++) {
-		intersects[i].object.material.color.set( 0xff0000 );
-	}
 
   renderer.render(scene, camera);
 };
